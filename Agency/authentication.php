c37b4cde81611b49777a34fe45319ef4c5d7f9dc
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-01-05 01:53:19 +0530

    Tables Updated

--- Agency/authentication.php
+++ Agency/authentication.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['loggedInStatus'])){
+if (!isset($_SESSION['loggedInStatus'])) {
 
     $_SESSION['message'] = "Login to continue...";
-    
-    header('Location: login.php');
+
+    header('Location: loginRegister.php');
     exit();
 }
 ?>
